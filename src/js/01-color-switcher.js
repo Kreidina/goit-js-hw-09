e03f5d162bb0edc4bb9bddaf01575bd6be46841d
Anna Kreidina <kav99mail@gmail.com>
2023-03-12 15:54:23 +0100

    1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const refs = {
+    start : document.querySelector('button[data-start]'),
+    stop : document.querySelector('button[data-stop]'),
+    bodyStyle : document.querySelector('body'),
+}
+
+refs.start.addEventListener('click', ()=> colorSwitch.onStart());
+refs.stop.addEventListener('click', ()=> colorSwitch.onStop());
+
+
+const colorSwitch = {
+    intervalId : null,
+    isActive : false,
+ onStart() {
+    if(this.isActive){
+        return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() =>{
+        refs.bodyStyle.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+},
+ onStop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+
+
+}
+}
 
