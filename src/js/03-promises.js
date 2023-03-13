3d80aee8cf4b31e64dfe38198cde89cf516d770f
Anna Kreidina <kav99mail@gmail.com>
2023-03-13 17:49:20 +0100

    завдання 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const refs = {
+  form : document.querySelector('.form'),
+  button : document.querySelector('button'),
 }
+
+refs.form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+  const delay = Number(refs.form.delay.value);
+  const step = Number(refs.form.step.value);
+  const amount = Number(refs.form.amount.value);
+
+  for(let i = 1; i <= amount; i ++){
+    createPromise(i, delay + (i - 1) * step)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  }
+})
+
+
+function createPromise(position, delay) {
+  return new Promise ((resolve, reject) =>{
+    setTimeout(() =>{
+      const shouldResolve = Math.random() > 0.3;
+       if (shouldResolve) {
+        resolve ({position, delay});
+    } else{
+      reject({position, delay})
+    }
+    }, delay);
+    });
+    };
